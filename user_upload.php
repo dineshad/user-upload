70d0262d4efc08b8d454201cea0a9f04e642b4a5
dinesha <dineshasenani@yahoo.com>
2022-01-13 10:40:17 +1100

    Adding create_table and help functionality

--- user_upload.php
+++ user_upload.php
@@ -1,11 +1,56 @@
 <?php
+/**
+ * A PHP script to parse a CSV and upload the value
+ * to MySQL database table.
+ * 
+ * Pseudocode:
+ * IF  notset (--file ) THEN 
+ *  IF  isset (--create_table ) THEN  
+ *      IF  isset (-h AND -u AND -p ) THEN 
+ *           create_users_table(h,u,p)
+ *      ELSE
+ *          print("Please retry with username,password and host for the database")
+ *      ENDIF
+ *  ENDIF
+ *  IF isset (--help )
+ *       print_help_message()
+ *  ENDIF
+ * ELSE
+ *  IF exist(filename)
+ *      set users_array = get_csv_to_array(filename)
+ *      IF sizeof(users_array) > 0
+ *          FOREACH(users_array)
+ *              ucfirst(first_name)
+ *              ucfirst(last_name)
+ *              strtolower(email)
+ *              IF (valid email)
+ *                  add to (valid_email_users_array)
+ *              ELSE
+ *                  add to (invalid_email_users_array)
+ *              ENDIF
+ *          END FOREACH
+ *          IF notset(--dry_run)
+ *              insert_to_db(valid_email_users_array)
+ *          ENDIF
+ *          print_valid_email_users_array
+ *          print_invalid_email_users_array
+ *      ELSE
+ *          print("No data in the file");
+ *      ENDIF
+ *  ELSE
+ *      print("Please save the csv file in import folder and retry.")
+ *  ENDIF
+ * 
+ * 
+ * @author     Dinesha Dayananda <dinesha.dayananda@gmail.com> 
+ * @version    1.0
+ */
 require "user_upload_functions.php";
 
 $shortopts  = "";
 $shortopts .= "u:";  
 $shortopts .= "p:";
 $shortopts .= "h:";
-
 
 $longopts  = array(
     "file:",
@@ -14,38 +59,36 @@ $longopts  = array(
     "help"        
 );
 $options = getopt($shortopts, $longopts);
-//var_dump($options);
-if (isset($options['file'])){
-    //echo 'file option is there' ;
+	    
+if (!isset($options['file'])){
+    if (isset($options['create_table']))
+    {
+        if ( isset($options['h']) && isset($options['u']) && isset($options['p']))
+        {
+            create_users_table($options['h'],$options['u'],$options['p']);
+        } 
+        else
+        { 
+            echo "Please retry with username,password and host for the database\n\n" ;
+        }
+    }
+    if (isset($options['help']))
+    {
+        print_help_message();
+    } 
+}
+else{
     $filename = "import/" . $options['file'];
     if (file_exists($filename))  {
-        //echo 'file exists' ;
+       
         $users = get_csv_to_array($filename);        
-        //var_dump($users);       
+              
 
     }    
     else
     {
         echo 'The CSV file doesn not exist in import folder.<br>' ;
     }
-
-}
-else{
-    //echo 'no file option';
-    if (isset($options['create_table']))
-    {
-        if ((isset($options['u'])&& isset($options['p']) && isset($options['h']) ))
-        {
-            create_users_table($options['u'],$options['p'],$options['h']);
-        }
-        else
-        {
-            echo "Please provide username,password and host for the database" ;
-        }
-    }
-    if (isset($options['help']))
-        print_help_message();
-
 }
 
 ?>
--- user_upload_functions.php
+++ user_upload_functions.php
@@ -1,23 +1,68 @@
 <?php
-function get_csv_to_array($filename)
-{
-    if (($open = fopen($filename, "r")) !== FALSE) 
-    {
-        while (($data = fgetcsv($open, 1000, ",")) !== FALSE) 
-        {        
-            $array[] = $data; 
+/**
+ * TRY connect_db
+ *  IF error(connect_db)
+ *      THROW EXCEPTION("Error creating users table.")
+ *  ELSE
+ *      IF table_exist(user)   
+ *          drop_table(user);
+ *          print "user table is dropped successfully.\n"
+ *      ELSE
+ *      TRY create_table
+ *          IF success(create_table)
+ *              PRINT("user table created successfully.)
+ *          ELSE
+ *              THROW EXCEPTION("Error creating user table")
+ *          ENDIF
+ *      CATCH EXCEPTION             
+ *      EXIT
+ * CATCH EXCEPTION 
+ * EXIT              
+ */
+function create_users_table($host,$username,$password){   
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);      
+    $dbname = "user_details";
+    $conn = new mysqli($host, $username, $password, $dbname);
+    try{
+        if ($conn->connect_error) {
+            throw new Exception("Connection failed:\n" . $conn->connect_error ."\n");
         }
-        fclose($open);
+        else{
+            $sql_drop = "DROP TABLE IF EXISTS user ";
+            if ($conn->query($sql_drop)) {
+                echo "user table is dropped successfully IF it existed.\n" ;           
+            }
+            $sql_create = "CREATE TABLE IF NOT EXISTS user (".
+                "user_id INT NOT NULL AUTO_INCREMENT, ".
+                "fname VARCHAR(30) NOT NULL,".
+                "lname VARCHAR(30) NOT NULL,".
+                "email VARCHAR(30) NOT NULL,".
+                "PRIMARY KEY(user_id),".
+                "UNIQUE KEY unique_email (email) )";
+                try{
+                if ($conn->query($sql_create) === TRUE) {
+                    echo "user table created successfully\n\n";
+                    $conn->close();
+                } else {
+                    throw new Exception("Error creating user table: " . $conn->error);
+                    $conn->close();                    
+                }}
+                catch(Exception $ne){
+                    echo $ne->getMessage();
+                    exit();
+                }       
+        }
     }
-   /*echo "<pre>";    
-    var_dump($array);
-    echo "</pre>";  */
-    return $array ;
+    catch(Exception $e){
+        echo $e->getMessage();
+        exit();
+    }  
 }
-function create_users_table($username,$password,$host)
-{
 
-}
+
+    
+
+
 function  print_help_message()
 {
     echo "\n----------------------------------------\n";
@@ -35,4 +80,19 @@ function  print_help_message()
     str_pad("\n",31) ."All other functions will be executed, but the database will not be altered or updated.\n\n";
     echo str_pad("--help",30). "Explains options/directives of user_upload.php\n\n"; 
 }
+// invalid will not be inserted to db
+
+// shift
+// function get_csv_to_array($filename)
+// {
+//     if (($open = fopen($filename, "r")) !== FALSE) 
+//     {
+//         while (($data = fgetcsv($open, 1000, ",")) !== FALSE) 
+//         {        
+//             $array[] = $data; 
+//         }
+//         fclose($open);
+//     }  
+//     return $array ;
 ?>
+
